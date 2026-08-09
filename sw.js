f3cbe9cf9608f9d3fabf7bf610a598eed5e11c7b
Pig Ote <pigote@Hops.local>
2026-08-09 11:06:30 -0400

    Add v39 version label to settings dialog

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grocery-v38';
+const CACHE = 'grocery-v39';
 const ASSETS = [
   './', './index.html', './manifest.json',
   './assets/style.css', './assets/store.js', './assets/app.js',
